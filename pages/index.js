8a129df73620261636c7bc5e0b3b473f97509f0e
mosesvk <kaumatule93@gmail.com>
2022-02-14 16:19:11 -0700

    fixing most of the home page implementing the Property and Layout jsx files and inputting them into the _app.js file

--- pages/index.js
+++ pages/index.js
@@ -2,68 +2,98 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 
-// import Property from '../components/Property';
+import Property from '../components/Property';
 import { baseUrl, fetchApi } from '../utils/fetchApi';
 
-const myLoader = ({src, width, height}) => {
-  return src
-}
+const myLoader = ({ src, width, height }) => {
+  return src;
+};
 
-export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
+export const Banner = ({
+  purpose,
+  title1,
+  title2,
+  desc1,
+  desc2,
+  buttonText,
+  linkName,
+  imageUrl,
+}) => (
   <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
-    <Image loader={myLoader} src={imageUrl} width={500} height={300} alt='banner' />
+    <Image
+      loader={myLoader}
+      src={imageUrl}
+      width={500}
+      height={300}
+      alt='banner'
+    />
     <Box p='5'>
-      <Text color='gray.500' fontSize='sm' fontWeight='medium'>{purpose}</Text>
-      <Text fontSize='3xl' fontWeight='bold'>{title1}<br />{title2}</Text>
-      <Text fontSize='lg' paddingTop='3' paddingBottom='3' color='gray.700'>{desc1}<br />{desc2}</Text>
+      <Text color='gray.500' fontSize='sm' fontWeight='medium'>
+        {purpose}
+      </Text>
+      <Text fontSize='3xl' fontWeight='bold'>
+        {title1}
+        <br />
+        {title2}
+      </Text>
+      <Text fontSize='lg' paddingTop='3' paddingBottom='3' color='gray.700'>
+        {desc1}
+        <br />
+        {desc2}
+      </Text>
       <Button fontSize='xl'>
-        <Link href={linkName}><a>{buttonText}</a></Link>
+        <Link href={linkName}>
+          <a>{buttonText}</a>
+        </Link>
       </Button>
     </Box>
   </Flex>
 );
 
 const Home = ({ propertiesForSale, propertiesForRent }) => {
-  console.log(propertiesForSale, propertiesForRent)
+  console.log(propertiesForSale, propertiesForRent);
 
   return (
     <Box>
-    <Banner
-      purpose='RENT A HOME'
-      title1='Rental Homes for'
-      title2='Everyone'
-      desc1=' Explore from Apartments, builder floors, villas'
-      desc2='and more'
-      buttonText='Explore Renting'
-      linkName='/search?purpose=for-rent'
-      imageUrl='https://images.unsplash.com/photo-1484154218962-a197022b5858?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1774&q=80'
-    />
-    {/* <Flex flexWrap='wrap'>
-      {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
-    </Flex> */}
-    <Banner
-      purpose='BUY A HOME'
-      title1=' Find, Buy & Own Your'
-      title2='Dream Home'
-      desc1=' Explore from Apartments, land, builder floors,'
-      desc2=' villas and more'
-      buttonText='Explore Buying'
-      linkName='/search?purpose=for-sale'
-      imageUrl='https://images.unsplash.com/photo-1605146769289-440113cc3d00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
-    />
-    {/* <Flex flexWrap='wrap'>
+      <Banner
+        purpose='RENT A HOME'
+        title1='Rental Homes for'
+        title2='Everyone'
+        desc1=' Explore from Apartments, builder floors, villas'
+        desc2='and more'
+        buttonText='Explore Renting'
+        linkName='/search?purpose=for-rent'
+        imageUrl='https://images.unsplash.com/photo-1484154218962-a197022b5858?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1774&q=80'
+      />
+      <Flex flexWrap='wrap'>
+        {propertiesForRent.map((property) => (
+          <Property property={property} key={property.id} />
+        ))}
+      </Flex>
+      <Banner
+        purpose='BUY A HOME'
+        title1=' Find, Buy & Own Your'
+        title2='Dream Home'
+        desc1=' Explore from Apartments, land, builder floors,'
+        desc2=' villas and more'
+        buttonText='Explore Buying'
+        linkName='/search?purpose=for-sale'
+        imageUrl='https://images.unsplash.com/photo-1605146769289-440113cc3d00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
+      />
+      <Flex flexWrap='wrap'>
       {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
-    </Flex> */}
-  </Box>
-  )
-;
-}
-
-;
+    </Flex>
+    </Box>
+  );
+};
 
 export async function getStaticProps() {
-  const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`);
-  const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`);
+  const propertyForSale = await fetchApi(
+    `${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`
+  );
+  const propertyForRent = await fetchApi(
+    `${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`
+  );
 
   return {
     props: {
