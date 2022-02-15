614b32566f246a596fad76553aeb4964e5056ef8
mosesvk <kaumatule93@gmail.com>
2022-02-14 20:25:32 -0700

    finishing up

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -14,11 +14,11 @@ const LeftArrow = () => {
         onClick={() => scrollPrev()}
         fontSize='2xl'
         cursor='pointer'
-        d={['none','none','none','block']}
+        d={['none', 'none', 'none', 'block']}
       />
     </Flex>
   );
-}
+};
 
 const RightArrow = () => {
   const { scrollNext } = useContext(VisibilityContext);
@@ -30,17 +30,35 @@ const RightArrow = () => {
         onClick={() => scrollNext()}
         fontSize='2xl'
         cursor='pointer'
-        d={['none','none','none','block']}
-    />
+        d={['none', 'none', 'none', 'block']}
+      />
     </Flex>
   );
-}
+};
 export default function ImageSrollbar({ data }) {
   return (
-    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} style={{ overflow: 'hidden' }} >
+    <ScrollMenu
+      LeftArrow={LeftArrow}
+      RightArrow={RightArrow}
+      style={{ overflow: 'hidden' }}
+    >
       {data.map((item) => (
-        <Box key={item.id} width='910px' itemId={item.id} overflow='hidden' p='1'>
-          <Image placeholder="blur" blurDataURL={item.url} alt='imageScroll' src={item.url} width={1000} height={500}  sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px" />
+        <Box
+          key={item.id}
+          width='910px'
+          itemId={item.id}
+          overflow='hidden'
+          p='1'
+        >
+          <Image
+            placeholder='blur'
+            blurDataURL={item.url}
+            alt='imageScroll'
+            src={item.url}
+            width={1000}
+            height={500}
+            sizes='(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px'
+          />
         </Box>
       ))}
     </ScrollMenu>
